65f52933cd3453d4a7948d91fe727f73119293ca
Federico Gorla <federicogorla@gmail.com>
2022-07-14 15:48:55 +0200

    Removed sudo (add user to dialup group)

--- deploy_on_metal.sh
+++ deploy_on_metal.sh
@@ -2,4 +2,4 @@
 make clean all RPI=4
 KERNEL=$(readlink -f build/kernel8.img)
 echo $KERNEL
-sudo java -jar chainloader/LagorBoot.jar /dev/ttyUSB0  $KERNEL
+java -jar chainloader/LagorBoot.jar /dev/ttyUSB0  $KERNEL
