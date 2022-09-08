d8ab775c9a88be6723f600c5606e040db7780c0e
Federico Gorla <federicogorla@gmail.com>
2022-09-08 10:51:35 +0200

    Forgotten file

--- src/mmu/Mmu.h
+++ src/mmu/Mmu.h
@@ -31,7 +31,7 @@
 
 /* memory attribute indirect register */
 #define MATTR_DEVICE_nGnRnE        0x0
-#define MATTR_NORMAL_NC            0x44
+#define MATTR_NORMAL_NC            0xFF
 #define MATTR_DEVICE_nGnRnE_INDEX  0
 #define MATTR_NORMAL_NC_INDEX      1
 #define MAIR_EL1_VAL               ((MATTR_NORMAL_NC << (8 * MATTR_NORMAL_NC_INDEX)) | MATTR_DEVICE_nGnRnE << (8 * MATTR_DEVICE_nGnRnE_INDEX))
